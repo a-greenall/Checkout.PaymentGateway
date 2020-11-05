2426e7927da50f2553517519f2d5b31fd1d6b00b
Adam Greenall <adam.greenall2@gmail.com>
2020-11-05 18:49:12 +0000

    Add payments controller with empty methods for now
    Remove WeatherForecast boilerplate classes

---
+++ Checkout.PaymentGateway.Api/Controllers/PaymentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Checkout.PaymentGateway.Api.Controllers
+{
+    [Route("api/payments")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        // GET api/payments/5
+        [HttpGet("{id}")]
+        public int Get(int id)
+        {
+            return id;
+        }
+
+        // POST api/payments
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
