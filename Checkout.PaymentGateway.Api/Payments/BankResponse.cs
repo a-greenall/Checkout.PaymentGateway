3736bec2dfadde6c839c399054c8e4a512099d7c
Adam Greenall <adam.greenall2@gmail.com>
2020-11-08 18:36:16 +0000

    Create a mock banking service

---
+++ Checkout.PaymentGateway.Api/Payments/BankResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Checkout.PaymentGateway.Api.Payments
+{
+    public class BankResponse<T>
+    {
+        public T Response { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+    }
+}
---
+++ Checkout.PaymentGateway.Api/Payments/MockBankingService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+
+namespace Checkout.PaymentGateway.Api.Payments
+{
+    public interface IBankingService
+    {
+        /// <summary>
+        /// Submits a payment request to the banking service.
+        /// </summary>
+        /// <param name="payment">The requested payment.</param>
+        /// <returns>A response including the payment ID and status.</returns>
+        public BankResponse<Guid> SubmitPayment(PaymentRequestDto payment);
+    }
+
+    /// <summary>
+    /// A simulated instance of a banking service.
+    /// </summary>
+    public class MockBankingService : IBankingService
+    {
+        /// <inheritdoc />
+        public BankResponse<Guid> SubmitPayment(PaymentRequestDto payment)
+        {
+            // This simulates a successful response.
+            return new BankResponse<Guid>
+            {
+                Response = Guid.NewGuid(),
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+    }
+}
